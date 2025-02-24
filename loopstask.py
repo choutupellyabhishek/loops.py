a0fd673847a1707c9f22dca725e09d40f923a3a0
choutupellyabhishek <choutpelly.abhishek@gmail.com>
2025-02-25 01:10:20 +0530

    Add files via upload

---
+++ loopstask.py
@@ -0,0 +1,23 @@
+
+'''
+number = int(input("enter number:"))
+print(f"mutiplication of up to {number}:")
+for n in range(1,number+1):
+    print(f"\nmultiplication table of{n}:")
+    for i in range(1,21):
+        print(f"{n}*{i} = {n*i}")
+'''
+'''
+for num in range(1, 101):
+    if  num % 3 ==0 and num %5 == 0 :
+        print(num)
+             '''
+
+'''
+number = int(input("enter a number:"))
+factorial = 1
+i = number
+while i > 0:
+    factorial *= i
+    i -= 1
+print(factorial)                 '''
